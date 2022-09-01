ab955493ca7fcbdd854e1c6fa5db9bfcf20288b3
PedroCeccon <p247327@dac.unicamp.br>
2022-08-31 21:07:08 -0300

    update lab03

--- lab3/lab03.c
+++ lab3/lab03.c
@@ -31,12 +31,12 @@ void write(int __fd, const void *__buf, int __n){
     );
 }
  
-int power(int base, int value, int position){
+int power(int base, int position){
     int result = 1;
     for (int i = 0; i < position; i++){
         result = result*base;
     }
-    return result*value;
+    return result;
 }
 
 void copyString(char *input, char* output, int n){
@@ -45,87 +45,84 @@ void copyString(char *input, char* output, int n){
     }
 }
 
-void invertString(char *input, char *output, int n){
+void invertString(char *input, char *output, int n, int start){
     for(int i = 0; i < n; i++){
-        output[i] = input[n-i-1];
+        output[i+start] = input[n-i-1];
     }
 }   
 
-int decToHex(char *decimal, char *hex, int n){
-    return 0;
+int intToString(unsigned int value, int base, int start, char *string){
+    int size = 0;
+    char temp[40];
+    int digit_value;
+
+    while(value > 0){
+        digit_value = value%base;
+        value -= digit_value;
+        value = value/base;
+        int ascii_complement = (digit_value<10 ? 48 : 87);
+        temp[size] = digit_value+ascii_complement;
+        size++;
+    }
+
+    invertString(temp, string, size, start);
+    size += start;
+    string[size] = 10;
+
+    return size + 1;
 }
 
-int decToBin(char *decimal, char *binary, int n){
-    return 0;
-}
-
-int hexToBin(char *hex, char *binary, int n){
-    int bin_size = 3;
-    int digit_value, bit_value;
-    for (int i = 2; i<n-1; i++){
-        if(hex[n-i] <= 57){
-            digit_value = hex[n-i]-48;
+int stringToInt(char *string, int size, int base, int start){
+    unsigned int digit_value, total_value=0;
+    for (int i = 2; (size - i) >= start; i++){
+        if(string[size-i] <= 57){
+            digit_value = string[size-i]-48;
         }
         else{
-            digit_value = hex[n-i]-87;
+            digit_value = string[size-i]-87;
         }
-        int counter = 0;
-        while(counter < 4){
-            bit_value = digit_value%2;
-            digit_value -= bit_value;
-            digit_value = digit_value/2;
-            binary[bin_size - counter] = bit_value+48;
-            counter++;
-        }
-        bin_size += 4;
+        total_value += digit_value*power(base, i-2);
     }
-    binary[bin_size] = 10;
-    return bin_size+1;
+    return total_value;
 }
 
-int hexToDec(char *hex, char *decimal, int n){
-    int decimal_size = 0;
-    char temp[20];
-    int digit_value, total_value = 0;
-    for (int i = 2; (n - i) >= 2; i++){
-        if(hex[n-i] <= 57){
-            digit_value = hex[n-i]-48;
-        }
-        else{
-            digit_value = hex[n-i]-87;
-        }
-        total_value += power(16, digit_value, i-2);
+void switchEndian(char *input, char *output, int input_size){
+    char temp[32];
+    for(int i = 1; i < input_size - 2; i++){
+        temp[32-i] = input[input_size-i-1];
     }
-
-    while(total_value > 0){
-        digit_value = total_value%10;
-        total_value -= digit_value;
-        total_value = total_value/10;
-        temp[decimal_size] = digit_value+48;
-        decimal_size++;
+    for(int i = input_size - 2; i <=32; i++){
+        temp[32-i] = '0';
     }
-
-    invertString(temp, decimal, decimal_size);
-    decimal[decimal_size] = 10;
-
-    return decimal_size + 1;
+    for(int i = 0; i < 32; i+=8){
+        for(int j = 0; j < 8; j++){
+            output[31-i-j] = temp[i+7-j];
+        }
+    }
 }
 
 int main(){
     char input[20];
     int n = read(0, input, 20);
-    char binary[40], endian_s[20], hex[20], decimal[20];
+    char binary[40], hex[20], decimal[20], endian_2[32], endian_10[20];
     int bin_size, decimal_size, hex_size, endian_size;
+    unsigned int total_value;
     if(input[0] == '0' && input[1] == 'x'){
         copyString(input, hex, n);
         hex_size = n;
-        decimal_size = hexToDec(hex, decimal, n);
-        bin_size = hexToBin(hex, binary, n);
+        total_value = stringToInt(hex, n, 16, 2);
+        decimal_size = intToString(total_value, 10, 0, decimal);
+        binary[0] = '0';
+        binary[1] = 'b';
+        bin_size = intToString(total_value, 2, 2, binary);
+        switchEndian(binary, endian_2, bin_size);
+        total_value = stringToInt(endian_2, 33, 2, 0);
+        endian_size = intToString(total_value, 10, 0, endian_10);
     }
-    write(1, input, n);
-    write(1, hex, hex_size);
-    write(1, decimal, decimal_size);
     write(1, binary, bin_size);
+    write(1, decimal, decimal_size);
+    write(1, hex, hex_size);
+    write(1, endian_10, endian_size);
     
     
     return 0;
--- lab3/test.c
+++ lab3/test.c
@@ -1,45 +1,107 @@
-int power(int base, int value, int position){
+//Nome: Pedro Henrique Peraçoli Pereira Ceccon
+//RA: 247327
+//Turma: MC404 B
+ 
+#include <stdio.h>
+
+int power(int base, int position){
     int result = 1;
     for (int i = 0; i < position; i++){
         result = result*base;
     }
-    return result*value;
+    return result;
 }
 
-int hexToDec(char *hex, char *decimal, int n){
-    int decimal_size = 0;
-    char temp[20];
-    int digit_value, total_value = 0;
-    for (int i = 2; (n - i) >= 2; i++){
-        if(hex[n-i] <= 57){
-            digit_value = hex[n-i]-48;
+void copyString(char *input, char* output, int n){
+    for(int i = 0; i < n; i++){
+        output[i] = input[i];
+    }
+}
+
+void invertString(char *input, char *output, int n, int start){
+    for(int i = 0; i < n; i++){
+        output[i+start] = input[n-i-1];
+    }
+}   
+
+int intToString(unsigned int value, int base, int start, char *string){
+    int size = 0;
+    char temp[40];
+    int digit_value;
+
+    while(value > 0){
+        digit_value = value%base;
+        value -= digit_value;
+        value = value/base;
+        int ascii_complement = (digit_value<10 ? 48 : 87);
+        temp[size] = digit_value+ascii_complement;
+        size++;
+    }
+
+    invertString(temp, string, size, start);
+    size += start;
+    string[size] = 10;
+
+    return size + 1;
+}
+
+int stringToInt(char *string, int size, int base, int start){
+    unsigned int digit_value, total_value=0;
+    for (int i = 2; (size - i) >= start; i++){
+        if(string[size-i] <= 57){
+            digit_value = string[size-i]-48;
         }
         else{
-            digit_value = hex[n-i]-87;
+            digit_value = string[size-i]-87;
         }
-        total_value += power(16, digit_value, i-2);
+        total_value += digit_value*power(base, i-2);
     }
-
-    while(total_value > 0){
-        digit_value = total_value%10;
-        total_value -= digit_value;
-        total_value = total_value/10;
-        temp[decimal_size] = digit_value+48;
-        decimal_size++;
-    }
-
-    for(int i = 0; i < decimal_size; i++){
-        decimal[i] = temp[decimal_size - i - 1];
-    }
-    decimal[decimal_size] = 10;
-
-    return decimal_size + 1;
+    return total_value;
 }
 
-void main(){
-    char decimal[20];
-    int decimal_size;
-    decimal_size = hexToDec("0x12\n", decimal, 5);
-    decimal[decimal_size] = "\0";
-    printf("%s", decimal);
+void switchEndian(char *input, char *output, int input_size){
+    char temp[32];
+    for(int i = 1; i < input_size - 2; i++){
+        temp[32-i] = input[input_size-i-1];
+    }
+    for(int i = input_size - 2; i <=32; i++){
+        temp[32-i] = '0';
+    }
+    for(int i = 0; i < 32; i+=8){
+        for(int j = 0; j < 8; j++){
+            output[31-i-j] = temp[i+7-j];
+        }
+    }
+}
+
+int main(){
+    char input[] = "0x545648\n";
+    int n = 9;
+    char binary[40], hex[20], decimal[20], endian_2[32], endian_10[20];
+    int bin_size, decimal_size, hex_size, endian_size;
+    unsigned int total_value;
+    if(input[0] == '0' && input[1] == 'x'){
+        copyString(input, hex, n);
+        hex_size = n;
+        total_value = stringToInt(hex, n, 16, 2);
+        decimal_size = intToString(total_value, 10, 0, decimal);
+        binary[0] = '0';
+        binary[1] = 'b';
+        bin_size = intToString(total_value, 2, 2, binary);
+        switchEndian(binary, endian_2, bin_size);
+        total_value = stringToInt(endian_2, 33, 2, 0);
+            endian_size = intToString(total_value, 10, 0, endian_10);
+        printf("%s", endian_10);
+    }
+    /*write(1, input, n);
+    write(1, hex, hex_size);
+    write(1, decimal, decimal_size);
+    write(1, binary, bin_size);*/
+    
+    
+    return 0;
+}
+ 
+void _start(){
+    main();
 }
