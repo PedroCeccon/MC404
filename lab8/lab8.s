61cd97cd5c6dfff87bf374b60d8dff91bbf1a656
Pedro Ceccon <p247327@dac.unicamp.br>
2022-11-22 22:59:59 -0300

    lab8 done and lab9

--- lab8/lab8.s
+++ lab8/lab8.s
@@ -6,9 +6,6 @@
 .data
 self_driving_car: .word 0xffff0100
 
-.bss
-dados: .skip 4000
-
 .text
 .globl _start
 _start:
@@ -16,39 +13,58 @@ _start:
     lw s0, 0(s0)
     li s10, 73      #x-final
     li s11, -19     #z-final
-    la s5, dados
 
-    getDirection:
-    jal getInitialData
-    jal setForward
-    jal getNewPosition
-    jal produtoInterno
-    bgtz s3, rightTurn
-    bltz s3, leftTurn
-    bnez s3, getDirection
+    getToGoal:
+        jal getInitialData
+        jal setForward
+        li a3, 3
 
-    jal stop
-    loop:
-    li a0, 5000
-    jal sleep
-    j loop
+        setDistance:
+        jal getNewPosition
+        jal goal
+        mul s6, s3, s3
+        mul s7, s4, s4
+        add s6, s6, s7
+        blt s6, a3, setDistance
+
+        jal break
+        jal getNewPosition
+        jal produtoInterno
+        li t6, -150
+        bge s3, t6, 1f
+            li t6, -15
+            jal turn
+            j 3f
+        1:
+        li t6, 150
+        ble s3, t6, 2f
+            li t6, 15
+            jal turn
+            j 3f
+        2:
+            mv t6, s3
+            li t0, 10
+            div t6, t6, t0
+            jal turn
+        3:
+        j getToGoal
 
 getInitialData:
-    li s1, 1
-    sb s1, 0x0(s0)
+    li t6, 1
+    sb t6, 0x0(s0)
     lw s1, 0x10(s0) #x-pos
     lw s2, 0x18(s0) #z-pos
-    sub s9, s1, s10 
+    sub s9, s10, s1 
     sub s8, s2, s11
     ret
 
 getNewPosition:
-    li s1, 1
-    sb s1, 0x0(s0)
-    lw s3, 0x10(s0) #x-pos
-    lw s4, 0x18(s0) #z-pos
-    sub s3, s3, s1
-    sub s4, s4, s2
+    li s5, 1
+    sb t6, 0x0(s0)
+    lw a6, 0x10(s0) #x-pos
+    lw a7, 0x18(s0) #z-pos
+    sub s3, a6, s1
+    sub s4, a7, s2
     ret
 
 produtoInterno:
@@ -60,74 +76,36 @@ produtoInterno:
 setForward:
     la s0, self_driving_car
     lw s0, 0(s0)
-    li s1, 1
+    li t6, 1
     sb zero, 0x22(s0)
-    sb s1, 0x21(s0)
-    ret
-
-leftTurn:
-    la s0, self_driving_car
-    lw s0, 0(s0)    
-    li s1, -70
-    sb s1, 0x20(s0)
-    ret
-
-rightTurn:
-    la s0, self_driving_car
-    lw s0, 0(s0)    
-    li s1, 15
-    sb s1, 0x20(s0)
+    sb t6, 0x21(s0)
     ret
 
 turn:
     la s0, self_driving_car
     lw s0, 0(s0)    
-    sb s1, 0x20(s0)
+    sb t6, 0x20(s0)
     ret
 
 break:
     la s0, self_driving_car
     lw s0, 0(s0)    
-    sb s1, 0x20(s0)
-    li s1, 1
+    sb t6, 0x20(s0)
+    li t6, 1
     sb zero, 0x21(s0)
-    sb s1, 0x22(s0)
+    sb t6, 0x22(s0)
     ret
 
 stop:
     jal break
     j stop
 
-time:
-    addi sp, sp, -16
-    mv a0, sp
-    mv a1, zero
-    li a7, 169
-    ecall
-    lw t0, 0(sp)
-    lw t1, 8(sp)
-    addi sp, sp, 16
-    li t2, 1000
-    mul t0, t0, t2
-    div t1, t1, t2
-    add a0, t0, t1
-    ret
-
-sleep:
-    addi sp, sp, -16
-    sw ra, 0(sp)
-    mv t3, a0
-    li t4, -1
-    jal time
-    bgtz a0, 8
-    mul a0, a0, t4
-    add t3, t3, a0
-    confere:
-    1:
-        jal time
-        bgtz a0, 8
-        mul a0, a0, t4
-        blt a0, t3, 1b
-    lw ra, 0(sp)
-    addi sp, sp, 16
+goal:
+    sub t0, a6, s10
+    sub t1, a7, s11
+    mul t0, t0, t0
+    mul t1, t1, t1
+    add t0, t0, t1
+    li t3, 1200
+    blt t0, t3, stop
     ret
